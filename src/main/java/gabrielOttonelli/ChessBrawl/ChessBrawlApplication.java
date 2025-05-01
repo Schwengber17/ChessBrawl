e18a0f409d270f939b9cf415687e28c9d052f91a
Schwengber17 <gabriel.schwengber@edu.pucrs.br>
2025-05-01 19:51:38 -0300

    Start

---
+++ src/main/java/gabrielOttonelli/ChessBrawl/ChessBrawlApplication.java
@@ -0,0 +1,13 @@
+package gabrielOttonelli.ChessBrawl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessBrawlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChessBrawlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gabrielOttonelli/ChessBrawl/ChessBrawlApplicationTests.java
@@ -0,0 +1,13 @@
+package gabrielOttonelli.ChessBrawl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessBrawlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
